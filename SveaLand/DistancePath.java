63105afbfc9a005612d9f1918abf416b42646623
MKL <MKL@DESKTOP-MRTBJQE>
2018-05-09 00:58:40 +0200

    ew måsvingar

--- SveaLand/DistancePath.java
+++ SveaLand/DistancePath.java
@@ -1,27 +1,20 @@
-
-public class DistancePath implements Comparable<DistancePath> 
-{
+public class DistancePath implements Comparable<DistancePath> {
 	public Vertex dest;
 	public double dist;
-	
-	public DistancePath(Vertex dest, double dist)
-	{
+
+	public DistancePath(Vertex dest, double dist) {
 		this.dest = dest;
 		this.dist = dist;
-		
+
 	}
-	public int compareTo(DistancePath d)
-	{
-		if(dist> d.dist)
-		{
+
+	public int compareTo(DistancePath d) {
+		if (dist > d.dist) {
 			return 1;
-		}
-		else if(dist<d.dist)
-		{
+		} else if (dist < d.dist) {
 			return -1;
-		}
-		else
+		} else
 			return 0;
-		
+
 	}
 }
